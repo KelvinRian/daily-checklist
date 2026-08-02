33a6db84fe1f0489c6cf79ec428239a76811877e
KelvinRian <kelvinlourenco18@gmail.com>
2026-08-02 00:13:37 -0300

    add CreateRoutineCommand

---
+++ backend/DailyChecklist/DailyChecklist.Application/Routine/Create/CreateRoutineCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyChecklist.Application.Routine.Create
+{
+    public class CreateRoutineCommand
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateOnly StartDate { get; set; }
+        public ICollection<CreateRoutineTaskDto> Tasks { get; set; }
+    }
+
+    public class CreateRoutineTaskDto 
+    {
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public CreateRoutineTaskGroupDto TaskGroup { get; set; }
+    }
+
+    public class CreateRoutineTaskGroupDto
+    {
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
---
+++ backend/DailyChecklist/DailyChecklist.Application/Routine/Create/CreateRoutineHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyChecklist.Application.Routine.Create
+{
+    public class CreateRoutineHandler
+    {
+        public async Task Handle(CreateRoutineCommand command)
+        {
+            // TODO
+        }
+    }
+}
