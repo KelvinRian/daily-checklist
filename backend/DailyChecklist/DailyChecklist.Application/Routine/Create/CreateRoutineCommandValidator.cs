49a1821db68c2dcb4ace464c558d4e1a82e30eef
KelvinRian <kelvinlourenco18@gmail.com>
2026-08-26 13:00:07 -0300

    Add FluentValidation

---
+++ backend/DailyChecklist/DailyChecklist.Application/Routine/Create/CreateRoutineCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace DailyChecklist.Application.Routine.Create
+{
+    public class CreateRoutineCommandValidator : AbstractValidator<CreateRoutineCommand>
+    {
+        public CreateRoutineCommandValidator()
+        {
+            AddRulesForName();
+            AddRulesForDescription();
+            //TODO
+            //Rules for Items
+        }
+
+        private void AddRulesForName()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("'Name' must not be empty.");
+            RuleFor(x => x.Name).MaximumLength(200).WithMessage("'Name' must not be greater than 200 characters.");
+        }
+
+        private void AddRulesForDescription()
+        {
+            RuleFor(x => x.Description).MaximumLength(500).WithMessage("'Description' must not be greater than 500 characters.");
+        }
+    }
+}
---
+++ backend/DailyChecklist/DailyChecklist.Tests/DailyChecklist.Application/Routine/Create/CreateRoutineCommandValidatorTests.cs
@@ -0,0 +1,105 @@
+﻿using DailyChecklist.Application.Routine.Create;
+
+namespace DailyChecklist.Tests.DailyChecklist.Application.Routine.Create
+{
+    public class CreateRoutineCommandValidatorTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Should_Not_Validate_NullOrEmpty_Name(string name)
+        {
+            // Arrange
+            var command = new CreateRoutineCommand
+            {
+                Name = name,
+            };
+            var validator = new CreateRoutineCommandValidator();
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e =>
+                e.PropertyName == "Name" &&
+                e.ErrorMessage == "'Name' must not be empty.");
+        }
+
+        [Fact]
+        public void Should_Not_Validate_Name_GreaterThan200Characters()
+        {
+            // Arrange
+            var command = new CreateRoutineCommand
+            {
+                Name = new string('a', 201),
+            };
+            var validator = new CreateRoutineCommandValidator();
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e =>
+                e.PropertyName == "Name" &&
+                e.ErrorMessage == "'Name' must not be greater than 200 characters.");
+        }
+
+        [Fact]
+        public void Should_Validate_200Characters_Name()
+        {
+            // Arrange
+            var command = new CreateRoutineCommand
+            {
+                Name = new string('a', 200),
+            };
+            var validator = new CreateRoutineCommandValidator();
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Should_Not_Valdiate_Description_GreaterThan500Characters()
+        {
+            // Arrange
+            var command = new CreateRoutineCommand
+            {
+                Name = "Valid Name",
+                Description = new string('a', 501),
+            };
+            var validator = new CreateRoutineCommandValidator();
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e =>
+                e.PropertyName == "Description" &&
+                e.ErrorMessage == "'Description' must not be greater than 500 characters.");
+        }
+
+        [Fact]
+        public void Should_Valdiate_500Characters_Description()
+        {
+            // Arrange
+            var command = new CreateRoutineCommand
+            {
+                Name = "Valid Name",
+                Description = new string('a', 500),
+            };
+            var validator = new CreateRoutineCommandValidator();
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
